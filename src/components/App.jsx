b756661a7f9ebc4f043d9b7f46337ee3d962f57f
Sam <samuel_morgan_@outlook.com>
2020-09-24 23:42:53 +0100

    Widget info class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="page">
       <div className="title">Popup Widgets</div>
+      <div className="widget-info">
+        <button>Popup 1</button>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
+          minus.
+        </div>
+      </div>
     </div>
   );
 }
