5b34f934930cc83b0879326af9a022fd0eb14212
Sam <samuel_morgan_@outlook.com>
2020-09-25 20:09:34 +0100

    Popups all individually made and labelled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,33 @@ import React, { useState } from "react";
 import "css/app.css";
 import Popup from "./Popup.jsx";
 
+const noOfWidgets = 10;
+
 function App() {
-  const [popupOpen, setPopupOpen] = useState(false);
+  const [widgetsOpen, setWidgetsOpen] = useState(
+    new Array(noOfWidgets).fill(false)
+  );
   return (
     <div className="page">
       <div className="title">Popup Widgets</div>
-      {[...Array(10).keys()].map((i) => (
+      {[...Array(noOfWidgets).keys()].map((i) => (
         <div className="widget-info">
-          <button onClick={() => setPopupOpen(!popupOpen)}>
+          <button
+            onClick={() => {
+              let newArr = [...widgetsOpen];
+              newArr[i] = !widgetsOpen[i];
+              setWidgetsOpen(newArr);
+            }}
+          >
             Popup {i + 1}
           </button>
           <div>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
             minus.
           </div>
+          <Popup open={widgetsOpen[i]} text={`Popup #${i + 1}`} />
         </div>
       ))}
-      <Popup open={popupOpen} />
     </div>
   );
 }
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "css/popup.css";
 
 const Popup = (props) => {
-  const { open } = props;
-  return <div className={`popup ${open ? "open" : ""}`}>Popup</div>;
+  const { open, text } = props;
+  return <div className={`popup ${open ? "open" : ""}`}>{text}</div>;
 };
 
 export default Popup;
