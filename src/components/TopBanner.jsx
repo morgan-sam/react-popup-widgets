db849cbab2ce247ee23a09f40d02ace69a32089c
Sam <samuel_morgan_@outlook.com>
2020-09-26 16:24:39 +0100

    Top banner popup opens

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "css/topBanner.css";
 
 const TopBanner = (props) => {
-  const { text } = props;
+  const { open, header, text, closePopup } = props;
   return (
-    <div className="top-banner">
+    <div className={`top-banner ${open ? "open" : ""}`}>
       <div>
         Sezzle is currently hiring!:{" "}
         <a href="https://sezzle.com/careers">learn more {">"}</a>
       </div>
       <div>{text}</div>
-      <button className="top-banner-close-btn">
+      <button className="top-banner-close-btn" onClick={closePopup}>
         <div className="cross-line" />
         <div className="cross-line" />
       </button>
