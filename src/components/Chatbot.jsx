5e02ff46600aac109072af4649541aaf2869656a
Sam <samuel_morgan_@outlook.com>
2020-09-28 10:15:54 +0100

    Outsput some dialogue with a delay

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -5,6 +5,7 @@ import icon from "img/customer-service.svg";
 
 const Chatbot = (props) => {
   const [lastActive, setLastActive] = useState(0);
+  const [conversation, setConversation] = useState([]);
   const { open, header, text, closePopup } = props;
 
   useEffect(() => {
@@ -15,13 +16,24 @@ const Chatbot = (props) => {
     }
   }, [open]);
 
+  useEffect(() => {
+    if (lastActive === 1) setConversation((cur) => [...cur, "Hey!"]);
+    if (lastActive === 3) setConversation((cur) => [...cur, "What's up?"]);
+    if (lastActive === 6)
+      setConversation((cur) => [...cur, "Do you need help with anything?"]);
+  }, [lastActive]);
+
+  console.log(conversation);
+
   return (
     <div className="corner-container bottom-left open">
       <div className={`popup ${open ? "open" : ""}`}>
         <PopupHeader {...{ header, closePopup }} />
-        <div className="body">
+        <div className="chatbot body">
           <img src={icon} className="chatbot-icon" />
-          <div>{lastActive}</div>
+          {conversation.map((el) => (
+            <div>{el}</div>
+          ))}
         </div>
       </div>
     </div>
