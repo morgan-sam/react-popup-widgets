594dd15efaee5509d27f7bc8b9a0b5ce2b91658d
Sam <samuel_morgan_@outlook.com>
2020-09-28 10:10:04 +0100

    Added basic timer to chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "css/chatbot.css";
 import PopupHeader from "components/PopupHeader";
 import icon from "img/customer-service.svg";
 
 const Chatbot = (props) => {
+  const [lastActive, setLastActive] = useState(0);
   const { open, header, text, closePopup } = props;
+
+  useEffect(() => {
+    if (open) {
+      setInterval(() => {
+        setLastActive((time) => time + 1);
+      }, 1000);
+    }
+  }, [open]);
+
   return (
     <div className="corner-container bottom-left open">
       <div className={`popup ${open ? "open" : ""}`}>
         <PopupHeader {...{ header, closePopup }} />
         <div className="body">
           <img src={icon} className="chatbot-icon" />
-          <div>{text}</div>
+          <div>{lastActive}</div>
         </div>
       </div>
     </div>
