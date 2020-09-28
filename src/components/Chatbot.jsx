808dd69edc5c7867d0f2cf454c81c85b6377c557
Sam <samuel_morgan_@outlook.com>
2020-09-28 10:01:25 +0100

    Added icon to chatbot skeleton

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import "css/chatbot.css";
 import PopupHeader from "components/PopupHeader";
+import icon from "img/customer-service.svg";
 
 const Chatbot = (props) => {
   const { open, header, text, closePopup } = props;
@@ -7,7 +9,10 @@ const Chatbot = (props) => {
     <div className="corner-container bottom-left open">
       <div className={`popup ${open ? "open" : ""}`}>
         <PopupHeader {...{ header, closePopup }} />
-        <div className="body">{text}</div>
+        <div className="body">
+          <img src={icon} className="chatbot-icon" />
+          <div>{text}</div>
+        </div>
       </div>
     </div>
   );
