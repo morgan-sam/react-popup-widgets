aca2b5429ac3e9aa137674dbe78454dafae2b8a2
Sam <samuel_morgan_@outlook.com>
2020-09-29 21:22:26 +0100

    Changed quarter on click

--- src/components/Dial.jsx
+++ src/components/Dial.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "css/dial.css";
 
 const Dial = () => {
+  const [quad, setQuad] = useState(0);
   return (
-    <div className="dial">
-      <div className="dial-dot"></div>
-    </div>
+    <div
+      className="dial"
+      onClick={() => setQuad((cur) => (cur + 1) % 4)}
+      style={{
+        backgroundImage: `linear-gradient(${
+          -90 + 90 * quad
+        }deg, transparent 50%, white 50%), linear-gradient(${
+          90 * quad
+        }deg, white 50%, transparent 50%)`,
+      }}
+    />
   );
 };
 
